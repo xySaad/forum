c54f9a7b17f19e5e57e0f8dfbffa90105892763a
Tounsadi Saad salah <tounsadisaadsalah>
2024-12-19 18:13:26 +0100

    add: login

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module forum
 
 go 1.23.4
 
-require github.com/mattn/go-sqlite3 v1.14.24 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	golang.org/x/crypto v0.31.0
+)
--- main.go
+++ main.go
@@ -1,38 +1,89 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	db "forum/database"
+	"log"
 	"net/http"
+
+	db "forum/database"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+func hand_register_get(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/register.html")
+}
+
+func hand_login_get(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/login.html")
+}
+
+func hand_login_post(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println("Error parsing form data: ", err)
+		return
+	}
+	db, err := sql.Open("sqlite3", "./forum.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	username := r.Form.Get("username")
+	email := r.Form.Get("email")
+	password1 := r.Form.Get("password")
+	query := "SELECT password FROM users WHERE username=? AND email=?"
+	row := db.QueryRow(query, username, email)
+	var password string
+	// Scan the result into a variable
+	err = row.Scan(&password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("No user found")
+		}
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(password), []byte(password1))
+	if err != nil {
+		w.Write([]byte("oh snap"))
+	} else {
+		w.Write([]byte("bim"))
+	}
+}
+
+func hand_register_post(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println("Error parsing form data: ", err)
+		return
+	}
+	username := r.Form.Get("username")
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+	i, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		fmt.Println("Error creating user: ", err)
+	}
+	// Create the user
+	err = db.CreateUser(username, email, string(i))
+	if err != nil {
+		fmt.Println("Error creating user: ", err)
+		return
+	}
+	// Redirect to the login page
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+}
 
 func main() {
 	db.InitDB()
-	http.HandleFunc("GET /register", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "templates/register.html")
-	})
-	http.HandleFunc("POST /api/register", func(w http.ResponseWriter, r *http.Request) {	
-		// Parse the form data
-		err := r.ParseForm()
-		if err != nil {
-			fmt.Println("Error parsing form data: ", err)
-			return
-		}
-		username := r.Form.Get("username")
-		email := r.Form.Get("email")
-		password := r.Form.Get("password")
-		// Create the user
-		err = db.CreateUser(username, email, password)
-		if err != nil {
-			fmt.Println("Error creating user: ", err)
-			return
-		}
-		// Redirect to the login page
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
-	})
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "templates/roadmap.html")
-	})
+	http.HandleFunc("GET /login", hand_login_get)
+	http.HandleFunc("POST /api/login", hand_login_post)
+	http.HandleFunc("GET /register", hand_register_get)
+	http.HandleFunc("POST /api/register", hand_register_post)
+	http.HandleFunc("/", home)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Error starting server: ", err)
