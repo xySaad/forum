8d3b5987aa2b1ba533bdb69f1e31614835399d7b
tsaadsal <tounsadisaadsalah>
2024-12-21 17:05:42 +0100

    fff

--- database/createUSER.go
+++ database/createUSER.go
@@ -1,21 +1,20 @@
-package db
+package forum
 
 import (
 	"database/sql"
-	"log"
+	"errors"
 )
 
 func CreateUser(username, email, password string) error {
-	// Open a connection to the SQLite database
 	db, err := sql.Open("sqlite3", "./forum.db")
 	if err != nil {
-		log.Fatal(err)
+		return errors.New("probleme accesing database")
 	}
 	defer db.Close()
 	query := "INSERT INTO users (username, email, password) VALUES (?, ?, ?)"
 	_, err = db.Exec(query, username, email, password)
 	if err != nil {
-		log.Fatal(err)
+		return errors.New("user already exists")
 	}
 	return nil
 }
