c779bcf336e3cced629769ed903370beba92b924
tsaadsal <tounsadisaadsalah>
2024-12-24 10:39:38 +0100

    commit

--- database/createUSER.go
+++ database/createUSER.go
@@ -3,6 +3,7 @@ package forum
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 )
 
 func CreateUser(username, email, password string) error {
@@ -14,7 +15,9 @@ func CreateUser(username, email, password string) error {
 	query := "INSERT INTO users (username, email, password) VALUES (?, ?, ?)"
 	_, err = db.Exec(query, username, email, password)
 	if err != nil {
+		fmt.Println(err)
 		return errors.New("user already exists")
 	}
+
 	return nil
 }
--- main.go
+++ main.go
@@ -14,9 +14,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 func main() {
 	db.InitDB()
 	http.HandleFunc("GET /login", authUtils.Hand_login_get)
-	http.HandleFunc("POST /login", authUtils.Hand_login_post)
+	http.HandleFunc("POST /api/login", authUtils.Hand_login_post)
 	http.HandleFunc("GET /register", authUtils.Hand_register_get)
-	http.HandleFunc("POST /register", authUtils.Hand_register_post)
+	http.HandleFunc("POST /api/register", authUtils.Hand_register_post)
 	http.HandleFunc("/", home)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
