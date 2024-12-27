3602e8d46ef59b603906306f88ce9d2d929dfa75
tsaadsal <tounsadisaadsalah@gmail.com>
2024-12-27 09:57:28 +0100

    commit okda

--- authUtils/auth.go
+++ authUtils/auth.go
@@ -3,12 +3,16 @@ package forum
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"unicode"
 
 	database "forum/database"
 
+	"github.com/gofrs/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -18,8 +22,13 @@ type User struct {
 	Password string `json:"password"`
 }
 
-func Auth(jwt string) (is_auth bool) {
-	if jwt == "" {
+func IsAuth(r *http.Request) (is_auth bool) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		return
+	}
+	uuid := cookie.Value
+	if uuid == "" {
 		return
 	}
 
@@ -27,8 +36,8 @@ func Auth(jwt string) (is_auth bool) {
 	if err != nil {
 		return
 	}
-	query := "SELECT  ( password) FROM users WHERE password= ( ?)"
-	_, err = db.Exec(query, jwt)
+	query := "SELECT  ( uuid) FROM users WHERE uuid= ( ?)"
+	_, err = db.Exec(query, uuid)
 	if err != nil {
 		return
 	}
@@ -37,26 +46,85 @@ func Auth(jwt string) (is_auth bool) {
 }
 
 func Hand_register_get(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	http.ServeFile(w, r, "templates/register.html")
 }
 
-func VerifyPassword(password string) (verified bool) {
-	if 8 <= len(password) || len(password) <= 64 {
-		verified = true
+func VerifyPassword(password string) error {
+	if len(password) < 8 {
+		return errors.New("password should be atleast 8 chars long")
 	}
-	return
+	return nil
 }
-func VerifyName(name string) (verified bool) {
+
+func VerifyName(name string) error {
+	if len(name) < 4 || len(name) > 18 {
+		return errors.New("Name-lenght should be between 4 and 18")
+	}
 	for _, char := range name {
-		if char<32 {
-			
+		if !unicode.IsGraphic(char) {
+			return errors.New("Name should only contain printable characters")
 		}
 	}
-	return
+	db, err := sql.Open("sqlite3", "./forum.db")
+	if err != nil {
+		return err
+	}
+	query := "SELECT  (username) FROM users WHERE username= ( ?)"
+	err = db.QueryRow(query, name).Scan()
+	if err != sql.ErrNoRows {
+		return errors.New("Name already exists")
+	}
+	return nil
 }
-func Verify(user User) (verified bool) {
 
-	return
+func VerifyEmail(Email string) error {
+	EmailParts := strings.Split(Email, "@")
+	if len(EmailParts) != 2 {
+		return errors.New("invalid email")
+	}
+	if len(EmailParts[0]) == 0 {
+		return errors.New("invalid email")
+	}
+	EmailParts = strings.Split(EmailParts[1], ".")
+	if len(EmailParts) != 2 {
+		return errors.New("invalid email")
+	}
+	if len(EmailParts[0]) == 0 {
+		return errors.New("invalid email")
+	}
+	for _, char := range Email {
+		if !unicode.IsGraphic(char) {
+			return errors.New("Email should only contain printable characters")
+		}
+	}
+	db, err := sql.Open("sqlite3", "./forum.db")
+	if err != nil {
+		return err
+	}
+
+	query := "SELECT  (email) FROM users WHERE email= ( ?)"
+	err = db.QueryRow(query, Email).Scan()
+	if err != sql.ErrNoRows {
+		return errors.New("Email already exists")
+	}
+	return nil
+}
+
+func Verify(user User) error {
+	err := VerifyName(user.Username)
+	if err != nil {
+		return err
+	}
+	err = VerifyPassword(user.Password)
+	if err != nil {
+		return err
+	}
+	err = VerifyEmail(user.Email)
+	return err
 }
 
 func Hand_register_post(w http.ResponseWriter, r *http.Request) {
@@ -64,35 +132,31 @@ func Hand_register_post(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	jwt, err := r.Cookie("token")
-	if err == nil {
-		if Auth(string(jwt.Value)) {
-			fmt.Println("user")
-			return
-		}
-	}
 	var user User
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 		http.Error(w, "invalid form", http.StatusNotAcceptable)
 		return
 	}
-	if user.Email == "" || user.Password == "" || user.Username == "" {
+	if err := Verify(user); err != nil {
+		fmt.Println(err)
+		return
 	}
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
-		fmt.Println("Error creating user: ", err)
+		fmt.Println(err)
+		return
 	}
-
-	err = database.CreateUser(user.Username, user.Email, string(hashedPassword))
+	uuid, err := uuid.NewV7()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = database.CreateUser(user.Username, user.Email, string(hashedPassword), uuid.String())
 	if err != nil {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-	Cookie := http.Cookie{
-		Name:  "token",
-		Value: string(hashedPassword),
-	}
-	http.SetCookie(w, &Cookie)
+	w.Header().Set("set-cookie", "token="+uuid.String())
 }
 
 func Hand_login_get(w http.ResponseWriter, r *http.Request) {
@@ -104,9 +168,13 @@ func Hand_login_get(w http.ResponseWriter, r *http.Request) {
 }
 
 func Hand_login_post(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		fmt.Println("Error parsing form data: ", err)
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "invalid form", http.StatusNotAcceptable)
 		return
 	}
 	db, err := sql.Open("sqlite3", "./forum.db")
@@ -114,20 +182,16 @@ func Hand_login_post(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	username := r.Form.Get("username")
-	email := r.Form.Get("email")
-	password1 := r.Form.Get("password")
 	query := "SELECT password FROM users WHERE username=? AND email=?"
-	row := db.QueryRow(query, username, email)
 	var password string
-	// Scan the result into a variable
-	err = row.Scan(&password)
+	err = db.QueryRow(query, user.Username, user.Email).Scan(&password)
 	if err != nil {
+
 		if err == sql.ErrNoRows {
 			fmt.Println("No user found")
 		}
 	}
-	err = bcrypt.CompareHashAndPassword([]byte(password), []byte(password1))
+	err = bcrypt.CompareHashAndPassword( []byte(user.Password),[]byte(password))
 	if err != nil {
 		w.Write([]byte("oh snap"))
 	} else {
--- database/createUSER.go
+++ database/createUSER.go
@@ -6,14 +6,14 @@ import (
 	"fmt"
 )
 
-func CreateUser(username, email, password string) error {
+func CreateUser(username, email, password, UUID string) error {
 	db, err := sql.Open("sqlite3", "./forum.db")
 	if err != nil {
 		return errors.New("probleme accesing database")
 	}
 	defer db.Close()
-	query := "INSERT INTO users (username, email, password) VALUES (?, ?, ?)"
-	_, err = db.Exec(query, username, email, password)
+	query := "INSERT INTO users (username, email, password,uuid) VALUES (?, ?, ?, ?)"
+	_, err = db.Exec(query, username, email, password, UUID)
 	if err != nil {
 		fmt.Println(err)
 		return errors.New("user already exists")
