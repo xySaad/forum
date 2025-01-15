c9f73a92a51c4a01ae5766bafefed7292dc8ff28
Mostafa Zakri <mostafazakri3@gmail.com>
2025-01-15 01:33:36 +0100

    feat(api): enhance authentication and error handling; add reaction module

--- app/handlers/auth_page.go
+++ app/handlers/auth_page.go
@@ -1,9 +1,20 @@
 package handlers
 
 import (
+	"forum/app/config"
 	"net/http"
 )
 
 func AuthPage(resp http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(resp, "405 - method not allowed", 405)
+		return
+	}
+	if req.URL.Path == "/auth/" || req.URL.Path == "/auth" {
+		config.Templates.Exec(resp, "login.html", nil)
+	} else {
+		http.Error(resp, "404 - page not found", 404)
+		return
+	}
 
 }
