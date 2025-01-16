7ee86ec9848d947539ffee2c51a12ecd505606f7
srm <saadrm.dev@gmail.com>
2025-01-16 14:13:21 +0100

    fix(ui): Improve responsive typography

--- static/js/components/Post.js
+++ static/js/components/Post.js
@@ -1,20 +1,23 @@
 import div from "./div.js";
 import img from "./img.js";
 import { timePassed } from "../utils/time.js";
+import Frame from "./Frame.js";
 
 const importSvg = (svgName) => "./static/svg/" + svgName + ".svg";
 
 const Post = (postData) =>
   div("postContainer").add(
-    div("post").add(
-      div("publisher").add(
-        img(importSvg("no-profile"), "no-profile"),
-        div(null, postData.publisher.name),
-        div(null, timePassed(postData.creationTime))
-      ),
-      div("title", postData.title),
-      div("text", postData.text),
-      div("readmore", "Read more")
+    Frame(
+      div("post").add(
+        div("publisher").add(
+          img(importSvg("no-profile"), "no-profile"),
+          div(null, postData.publisher.name),
+          div(null, timePassed(postData.creationTime))
+        ),
+        div("title", postData.title),
+        div("text", postData.text),
+        div("readmore", "Read more")
+      )
     ),
     div("leftBar").add(
       img(importSvg("arrow-up"), "arrow-up"),
@@ -22,5 +25,4 @@ const Post = (postData) =>
       img(importSvg("arrow-down"), "arrow-down")
     )
   );
-
 export default Post;
