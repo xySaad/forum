6f1c2aeb8766b22c64f3e80b6f1e1f6ecb5bc8b3
0Emperor <tounsadisaadsalah@gmail.com>
2025-01-18 13:39:12 +0100

    fix: add comment

--- app/api/comments/adder.go
+++ app/api/comments/adder.go
@@ -5,25 +5,34 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
+	db "forum/app/database"
+	"forum/app/modules"
 )
 
-func AddComment(data io.ReadCloser) error {
+func AddComment(conn *modules.Connection) error {
 	var comment Comment
-	err := json.NewDecoder(data).Decode(&comment)
+	err := json.NewDecoder(conn.Req.Body).Decode(&comment)
 	if err != nil {
 		return errors.New("invalid data format")
 	}
+	cookie, err := conn.Req.Cookie("token")
+	if err != nil || cookie.Value == "" {
+		return errors.New("unotorized")
+	}
+	uId, err := db.GetUserIDByToken(cookie.Value)
+	if err != nil {
+		return err
+	}
+
 	db, err := sql.Open("sqlite3", "forum.db")
 	if err != nil {
 		return errors.New("internal pointer variable")
 	}
 	query := `INSERT INTO comments (post_id, user_id, content) VALUES (?, ?, ?)`
-	_, err = db.Exec(query, comment.Post_id, comment.User_id, comment.Content)
-if err!=nil {
-	return errors.New("internal pointer variable")
+	_, err = db.Exec(query, comment.Post_id, uId, comment.Content)
+	if err != nil {
+		return errors.New("internal pointer variable")
+	}
+	fmt.Printf("comment: %v\n", comment)
+	return nil
 }
-fmt.Printf("comment: %v\n", comment)
-return  nil
-}
- 
