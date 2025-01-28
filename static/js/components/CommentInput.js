1b45956314def9f42c2fd01ab0fb391d41d810f4
xySaad <saadrm.dev@gmail.com>
2025-01-28 15:08:58 +0100

    feat: create comment

--- static/js/components/CommentInput.js
+++ static/js/components/CommentInput.js
@@ -2,17 +2,36 @@ import { importSvg } from "../utils/index.js";
 import div from "./native/div.js";
 import img from "./native/img.js";
 
-export const CommentInput = () => {
+export const CommentInput = (postID) => {
   const input = document.createElement("input");
-  const button = document.createElement("button");
-  button.append(img(importSvg("arrow-up")));
-  button.onclick = async () => {
-    const body = {};
+  input.placeholder = "Write a comment...";
+  const sendComment = async () => {
+    if (input.value.length == 0) {
+      return;
+    }
+    const body = {
+      post_id: postID,
+      content: input.value,
+    };
     const resp = await fetch("/api/coments", {
       method: "post",
       body: JSON.stringify(body),
     });
+    if (resp.ok) {
+      input.value = "";
+    }
   };
+
+  const button = document.createElement("button");
+  button.append(img(importSvg("arrow-up")));
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      sendComment();
+    }
+  });
+
+  button.onclick = sendComment;
 
   return div("inputwrap").add(input, button);
 };
