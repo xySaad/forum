a212d50431ab682ebbad61238c89637a4e1c174d
tsaadsal <tounsadisaadsalah@gmail.com>
2025-01-29 14:41:49 +0100

    minor changes

--- app/api/comments/struct.go
+++ app/api/comments/struct.go
@@ -1,9 +1,14 @@
 package comments
 
+import "forum/app/modules"
+
 type Comment struct {
 	UserID    string `json:"user_id"`
+	Publisher modules.User `json:"publisher"`
 	PostID    string `json:"post_id"`
 	ItemID    string `json:"item_id"`
 	Content   string `json:"content"`
+	Likes     int    `json:"likes"`
+	Dislikes  int    `json:"dislike"`
 	CreatedAt string `json:"created_at"`
 }
--- static/js/components/CommentsList.js
+++ static/js/components/CommentsList.js
@@ -1,7 +1,14 @@
 import div from "./native/div.js";
+let offset=0
 
 const getComments = async (postId, commentsList) => {
-  const resp = await fetch(`/api/coments?p_id=${postId}&offset=10`);
+  const resp = await fetch(`/api/coments?p_id=${postId}&offset=${offset}`);
+  const json = await resp.json()
+  offset=offset+json.length
+  
+json.forEach(comment => {
+  commentsList.append(comment.content)
+});
 };
 
 export const CommentsList = (postId) => {
