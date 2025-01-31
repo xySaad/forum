e7ef7f4e592b17069d967f34df554a8c0689fb33
0Emperor <tounsadisaadsalah@gmail.com>
2025-01-31 17:27:28 +0100

    some changes

--- static/js/home.js
+++ static/js/home.js
@@ -4,25 +4,9 @@ import { Reaction } from "./reactions.js";
 import CreatePost from "./components/createPost.js";
 import { filterCat } from "./filter.js";
 import ensureAuth from "./utils/ensureAuth.js";
-let page=0
+import { CreatePostsArea } from "./components/NewPost.js";
 export const Home = async () => {
-  try {
-    const resp = await fetch(`/api/posts?page=${page}&categories=0000`);
-    if (!resp.ok) {
-      console.log("Didn't get posts from api");
-      return;
-    }else{
-      page+=1
-    }
-    const posts = await resp.json();
-
-    const postsElement = document.querySelector(".posts");
-    posts.forEach((post) => {
-      postsElement.append(Post(post));
-    });
-  } catch (error) {
-    console.error(error);
-  }
+  CreatePostsArea()
   onResize(AdjustPostLines);
   Reaction();
   document.getElementById("create-post-btn").onclick = async () => {
