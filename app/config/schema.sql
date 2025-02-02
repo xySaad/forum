3f840662cb24f3ebdb5191ad43954cb517601981
0Emperor <tounsadisaadsalah@gmail.com>
2025-02-02 15:12:08 +0100

    updated categories table

--- app/config/schema.sql
+++ app/config/schema.sql
@@ -13,24 +13,31 @@ CREATE TABLE IF NOT EXISTS posts (
     user_id TEXT,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
-    categories TEXT,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
+CREATE TABLE IF NOT EXISTS post_categories (
+    post_id INTEGER,
+    category_id INTEGER,
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
 CREATE TABLE IF NOT EXISTS comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     post_id INTEGER NOT NULL,
     user_id TEXT NOT NULL,
     content TEXT NOT NULL,
-    likes INTEGER NOT NULL ,
-    dislikes INTEGER NOT NULL,
+    likes INTEGER NOT NULL DEFAULT 0,
+    dislikes INTEGER NOT NULL DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
+);
 CREATE TABLE IF NOT EXISTS reactions (
     item_id TEXT NOT NULL,
     user_id TEXT NOT NULL,
@@ -39,3 +46,7 @@ CREATE TABLE IF NOT EXISTS reactions (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     UNIQUE (user_id, item_id)
 );
+INSERT OR IGNORE INTO categories (id, name) VALUES (1,"Sport");
+INSERT OR IGNORE INTO categories (id, name) VALUES (2,"Finance");
+INSERT OR IGNORE INTO categories (id, name) VALUES (3,"Technology");
+INSERT OR IGNORE INTO categories (id, name) VALUES (4,"Science");
