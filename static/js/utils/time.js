f84ceefeddf6586fb00d0d90d59a0c04fb911c2c
srm <saadrm.dev@gmail>
2025-02-02 17:51:27 +0100

    fix: support months/weeks/years in time package

--- static/js/utils/time.js
+++ static/js/utils/time.js
@@ -7,6 +7,9 @@ export function timePassed(date) {
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(weeks / 4);
+  const years = Math.floor(months / 12);
 
   if (seconds < 60) {
     return `${seconds} sec ago`;
@@ -14,7 +17,13 @@ export function timePassed(date) {
     return `${minutes} mins ago`;
   } else if (hours < 24) {
     return `${hours} hours ago`;
-  } else {
+  } else if (days < 7) {
     return `${days} days ago`;
+  } else if (weeks < 4) {
+    return `${weeks} weeks ago`;
+  } else if (months < 12) {
+    return `${months} monts ago`;
+  } else {
+    return `${years} years ago`;
   }
 }
