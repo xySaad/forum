1be820cdb4587db9ecf6733ab8e8eebc32a02dc2
srm <saadrm.dev@gmail>
2025-02-02 17:52:48 +0100

    fix: unnecessary requests by img element

--- static/js/utils/index.js
+++ static/js/utils/index.js
@@ -1,5 +1,5 @@
 import { onResize } from "./events.js";
 import { timePassed } from "./time.js";
-const importSvg = (svgName) => "./static/svg/" + svgName + ".svg";
+const importSvg = (svgName) => svgName ? "./static/svg/" + svgName + ".svg" : "";
 
 export { onResize, timePassed, importSvg };
