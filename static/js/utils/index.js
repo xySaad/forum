9aafe2ae9cd3626be664222b2b15ca0a4da4c9e1
srm <saadrm.dev@gmail.com>
2025-02-06 22:50:16 +0100

    fix: invalid path in import svg

--- static/js/utils/index.js
+++ static/js/utils/index.js
@@ -1,5 +1,5 @@
 import { onResize } from "./events.js";
 import { timePassed } from "./time.js";
 const importSvg = (svgName) =>
-  svgName ? "./static/svg/" + svgName + ".svg" : "";
+  svgName ? "/static/svg/" + svgName + ".svg" : "";
 export { onResize, timePassed, importSvg };
