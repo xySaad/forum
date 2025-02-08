46460801b58fac403975daf1308b8d13a32a19b7
0Emperor <tounsadisaadsalah@gmail.com>
2025-02-08 12:04:25 +0100

    updated reaction for comment

--- app/api/comments/struct.go
+++ app/api/comments/struct.go
@@ -6,9 +6,10 @@ type Comment struct {
 	UserID       string       `json:"user_id"`
 	Publisher    modules.User `json:"publisher"`
 	PostID       string       `json:"post_id"`
-	ItemID       string       `json:"item_id"`
+	ItemID       int       `json:"item_id"`
 	Content      string       `json:"content"`
 	Likes        int          `json:"likes"`
 	Dislikes     int          `json:"dislikes"`
+	Reaction     int          `json:"reaction"`
 	CreationTime string       `json:"creationTime"`
 }
