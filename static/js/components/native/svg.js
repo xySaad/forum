0762f1d4af926f9603ee34c46668f224c4bf0880
xySaad <saadrm.dev@gmail.com>
2025-04-09 15:48:01 +0100

    feat: cache svg

--- static/js/components/native/svg.js
+++ static/js/components/native/svg.js
@@ -1,16 +1,38 @@
 import { importSvg } from "../../utils/index.js";
+const svgCache = new Map();
 const parser = new DOMParser();
 
 export const svg = (name) => {
-  const svg = document.createElement("svg");
-  svg.classList.add(name);
-  
-  fetch(importSvg(name)).then(async (res) => {
-    const text = await res.text();
-    const svgDoc = parser.parseFromString(text, "image/svg+xml");
-    const parsedSvg = svgDoc.firstChild;
-    svg.onload?.(parsedSvg);
-    svg.replaceWith(parsedSvg);
-  });
-  return svg;
+  const svgElem = document.createElement("svg");
+  const replaceSvg = (parsedSvg) => {
+    svgElem.onload?.(parsedSvg);
+    svgElem.replaceWith(parsedSvg.cloneNode(true));
+  };
+
+  svgElem.classList.add(name);
+  const url = importSvg(name);
+  const cachedSvg = svgCache.get(url);
+
+  if (!cachedSvg) {
+    const promise = fetchAndParseSvg(url);
+    promise.then(replaceSvg);
+    svgCache.set(url, promise);
+    return svgElem;
+  }
+  if (cachedSvg instanceof Promise) {
+    cachedSvg.then(replaceSvg);
+    return svgElem;
+  } else {
+    return cachedSvg.cloneNode(true);
+  }
+};
+
+const fetchAndParseSvg = async (url) => {
+  const resp = await fetch(url);
+  const text = await resp.text();
+  const svgDoc = parser.parseFromString(text, "image/svg+xml");
+  const parsedSvg = svgDoc.firstChild;
+  svgCache.set(url, parsedSvg);
+
+  return parsedSvg;
 };
