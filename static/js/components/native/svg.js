eb6cd901f1ca6300c9c2efd18119a344e8c096fb
xySaad <saadrm.dev@gmail.com>
2025-04-09 13:32:32 +0100

    fix: Improve svg wrapper

--- static/js/components/native/svg.js
+++ static/js/components/native/svg.js
@@ -1,9 +1,16 @@
 import { importSvg } from "../../utils/index.js";
+const parser = new DOMParser();
 
 export const svg = (name) => {
   const svg = document.createElement("svg");
+  svg.classList.add(name);
+  
   fetch(importSvg(name)).then(async (res) => {
-    svg.outerHTML = await res.text();
+    const text = await res.text();
+    const svgDoc = parser.parseFromString(text, "image/svg+xml");
+    const parsedSvg = svgDoc.firstChild;
+    svg.onload?.(parsedSvg);
+    svg.replaceWith(parsedSvg);
   });
   return svg;
 };
