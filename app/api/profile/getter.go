27d87491bf7707b1eff969f2acb535bb7c6e8e37
xySaad <saadrm.dev@gmail.com>
2025-04-20 17:10:59 +0100

    feat: fetch all user data "/api/profile"

--- app/api/profile/getter.go
+++ app/api/profile/getter.go
@@ -2,38 +2,30 @@ package profile
 
 import (
 	"database/sql"
-	"encoding/json"
-	"net/http"
 
 	"forum/app/modules"
+	"forum/app/modules/errors"
 	"forum/app/modules/log"
 )
+
+type privateUser struct {
+	modules.User
+	Email string `json:"email"`
+}
 
 func GetUserData(conn *modules.Connection, forumDB *sql.DB) {
 	if !conn.IsAuthenticated(forumDB) {
 		return
 	}
+	user := privateUser{User: conn.User}
 
-	type User struct {
-		Username string `json:"username"`
-		Email    string `json:"email"`
-	}
-
-	var userD User
-	qurr := `SELECT username, email FROM users WHERE id = ?`
-	err := forumDB.QueryRow(qurr, conn.User).Scan(&userD.Username, &userD.Email)
+	query := `SELECT username,profile_picture,email FROM users WHERE id=?`
+	err := forumDB.QueryRow(query, user.Id).Scan(&user.Username, &user.ProfilePicture, &user.Email)
 	if err != nil {
 		log.Error(err)
-		http.Error(conn.Resp, "Database query error", http.StatusInternalServerError)
+		conn.Error(errors.HttpInternalServerError)
 		return
-	}
-	conn.Resp.Header().Set("Content-Type", "application/json")
-	conn.Resp.WriteHeader(http.StatusOK)
 
-	jsond, err := json.Marshal(userD)
-	if err != nil {
-		http.Error(conn.Resp, "Error in JSON encoding", http.StatusInternalServerError)
-		return
 	}
-	conn.Resp.Write(jsond)
+	conn.Respond(user)
 }
