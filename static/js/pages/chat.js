933c029a17990efd22362b402916b06b81e6968d
Your Name <you@example.com>
2025-04-22 16:38:56 +0100

    first commit

--- static/js/pages/chat.js
+++ static/js/pages/chat.js
@@ -9,43 +9,64 @@ export const Chat = () => {
   const chatBubble = document.querySelector(".chat-bubble");
   toggleIcon(".chat-bubble");
   chatBubble.onload = (svg) => svg.classList.add("active");
-  return infinitusers("/api/users");
+  return div("boody").add((infinitusers("/api/users")), conversation())
 };
 
 export const infinitusers = (url) => {
   const activeUsers = div("users");
   getusers(activeUsers, url);
-  return activeUsers;
+  return activeUsers
 };
 
-export async function getusers(activeUsers, url) {
-  const [resp, err] = await resolve(Fetch(url));
-  if (err != null) {
-    console.error(err);
-    return;
-  }
+export async function getusers(UserArea, url) {
+  //let nextUrl
+  let json
+  try {
+    const resp = await fetch(url);
+    if (resp.status === 401) {
+      go("/login");
+      document.getElementsByClassName
+      return;
+    }
+    if (!resp.ok) {
+      throw new Error("status not ok:", resp.status);
+    }
+    json = await resp.json();
+    if (json) {
 
-  const json = await resp.json();
-  json.forEach((user) => {
-    activeUsers.add(
-      div("userholder").add(
-        div("profilepic").add(img(user.profilePicture, "no-profile")),
-        div("username", user.username)
-      )
-    );
-  });
+      for (const user of json) {
+        let img = document.createElement("img");
+        img.src = user.profilePicture;
+        let jj = div("userholder")
+        //jj.onclick = conversation
+        jj.add(
+          div("profilepic").add(img),
+          user.username
+        )
+        UserArea.append(jj);
+      }
+      nextUrl = new URL(resp.url);
+    }
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
 }
 export function conversation() {
-  return callfunction(infinitusers);
-}
-export function callfunction(func1) {
-  func1();
-  let creatbtn = document.createElement("button");
-  creatbtn.value = "send";
-  let inputmsg = input("msj-input", "message", true);
+  //return callfunction(infinitusers)
+  //func1()
+  let creatbtn = document.createElement("button")
+
+  let inputmsg = input("msj-input", "message", true)
   const conversation = div("ConversationArea").add(
-    div(msgdiv),
-    div("inputmsg").add(inputmsg, creatbtn)
-  );
-  return conversation;
+    div("msgdiv"),
+    div("inputmsg").add(inputmsg, creatbtn),
+  )
+  let test = document.querySelector("inputmsg")
+  /*   if (test !== null){
+      test.replaceWith(conversation)
+    }else {
+      document.querySelector("main").append(conversation);
+    }
+   */
+  return conversation
 }
