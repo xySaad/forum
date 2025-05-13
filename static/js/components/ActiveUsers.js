65e2d926bf4942edf3d6bf60826bf0442b357fa6
xySaad <saadrm.dev@gmail.com>
2025-05-13 13:32:52 +0100

    feat: better active users menu styles

--- static/js/components/ActiveUsers.js
+++ static/js/components/ActiveUsers.js
@@ -1,11 +1,21 @@
 import users from "../context/users.js";
 import { Fetch } from "../utils/fetch.js";
 import div from "./native/div.js";
+import { svg } from "./native/svg.js";
 import { UserCard } from "./UserCard.js";
 const USERS_API = "/api/users";
 
 export const ActiveUsers = async () => {
-  const usersContainer = div("users").add(div("title", "Users"));
+  const chevronLeft = svg("chevron-left");
+  const icon = svg("groups");
+
+  const usersContainer = div("users").add(
+    div("head").add(icon, div("title", "Users"), chevronLeft)
+  );
+  const hideUsers = () => usersContainer.classList.toggle("hide");
+  chevronLeft.onclick = hideUsers;
+  icon.onclick = hideUsers;
+
   if (users.size === 0) {
     const resp = await Fetch(USERS_API);
     if (resp.ok) {
